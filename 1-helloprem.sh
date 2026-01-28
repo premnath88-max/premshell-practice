6a97434511a38ef92331dd777281fc9ef6c97b93
Premnath <premnathb77@gmail.com>
2026-01-28 23:49:59 +0530

    Modified

--- 1-helloprem.sh
+++ 1-helloprem.sh
@@ -2,4 +2,8 @@
 name="Prem"
 age="32"
 city="Chintal"
- 
+
+echo "My name is $name. I am $age years old and I live in $city." 
+echo "I love programming in Shell Scripting!"
+echo "This is my first script."
+
