e62f4c9cce5046144b456ea77c441640afd532ca
Premnath <premnathb77@gmail.com>
2026-01-28 23:29:14 +0530

    Modified

--- 2prem.sh
+++ 2prem.sh
@@ -5,3 +5,16 @@ echo "Hello, Prem how are you!"
 echo " I am fine"
 
 
+echo "Enter a number:"
+read num
+
+if [ "$num" -gt 50 ]; then
+    echo "The number is greater than 50"
+
+    
+else
+    echo "The number is not greater than 50"
+fi
+
+
+
