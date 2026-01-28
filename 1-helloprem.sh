7fe26636d4534bb08965593a0e4faafa2e1f8d82
Premnath <premnathb77@gmail.com>
2026-01-28 23:42:49 +0530

    Modified

--- 1-helloprem.sh
+++ 1-helloprem.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+echo "Enter a number:"
+read num1
 
-echo "Hello, World!"
+echo "Enter another number:"
+read num2   
+sum=$((num1 + num2))
+div=$((num1 / num2))
+quotient=$((num1 % num2))
+sub=$((num1 - num2))
+
+echo  "Addition: $sum"
+echo  "Division: $div"
+echo  "Quotient: $quotient"
+echo  "Subtraction: $sub"
 
