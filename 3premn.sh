214e6bc97cf11ce318cecee23c1d97d47ad46235
Premnath <premnathb77@gmail.com>
2026-01-28 23:36:22 +0530

    Modified

---
+++ 3premn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "enter first number"
+read num1
+
+echo "enter second number"
+read num2
+
+sum=$((num1 + num2))
+diff=$((num1 - num2))
+Prod=$((num1 * num2))
+quot=$((num1 / num2))
