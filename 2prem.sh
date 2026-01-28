867c75994741f1a8579be249a6666d168a75a302
Premnath <premnathb77@gmail.com>
2026-01-28 23:34:49 +0530

    Modified

--- 2prem.sh
+++ 2prem.sh
@@ -16,5 +16,17 @@ else
     echo "The number is not greater than 50"
 fi
 
+echo "enter first number"
+read num1
+
+echo "enter second number"
+read num2
+
+sum=$((num1 + num2))
+diff=$((num1 - num2))
+Prod=$((num1 * num2))
+quot=$((num1 / num2))
+
+
 
 
