713501f72112a10880796441ecd4ffdda5352376
Premnath <premnathb77@gmail.com>
2026-01-28 23:51:55 +0530

    Modified

--- 1-helloprem.sh
+++ 1-helloprem.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-name="Prem"
-age="32"
-city="Chintal"
+a=12
+b=6
 
-echo "My name is $name. I am $age years old and I live in $city." 
-echo "I love programming in Shell Scripting!"
-echo "This is my first script."
+echo "Sum: $((a + b))"
+echo "Difference: $((a - b))"
+echo "Product: $((a * b))"
+echo "Quotient: $((a / b))"
+echo "Remainder: $((a % b))"
+
+if [ $a -gt $b ]; then
+    echo "$a is greater than $b"
+else
+    echo "$a is not greater than $b"
+fi
+echo "Enter your name:"
+read name
+echo "Hello, $name!"
+
+# This is a comment
+
+echo "This is a simple shell script."
 
