d9ab14989d18fe7fda6efdbc6960c5ebc0228ebb
Premnath <premnathb77@gmail.com>
2026-01-28 23:53:01 +0530

    Modified

--- 1-helloprem.sh
+++ 1-helloprem.sh
@@ -17,6 +17,14 @@ echo "Enter your name:"
 read name
 echo "Hello, $name!"
 
+echo "Enter your age:"
+read age
+if [ "$age" -ge 18 ]; then
+    echo "You are eligible to vote."
+else
+    echo "You are not eligible to vote."
+fi
+
 # This is a comment
 
 echo "This is a simple shell script."
