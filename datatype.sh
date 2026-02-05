ad3b2f733cc2f65b3d4e6aff337ef26f1b09f8a8
Premnath <premnathb77@gmail.com>
2026-02-05 20:30:21 +0530

    Modified

--- datatype.sh
+++ datatype.sh
@@ -3,6 +3,9 @@
 
 #Array
 
-FRUITS=("apple"  "banana"  "Orange")
+FRUITS=("apple"  "banana"  "Orange" "Guava")
 
-echo "All fruits are : ${FRUITS[@]}"
+echo "First fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+echo "Fourth fruit is: ${FRUITS[3]}"
