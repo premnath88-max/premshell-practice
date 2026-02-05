6dfa58a4b2e950e7ad34be6510e559e5d7683016
Premnath <premnathb77@gmail.com>
2026-02-05 21:48:02 +0530

    Modified

--- datatype.sh
+++ datatype.sh
@@ -6,6 +6,11 @@ if [ $NUM -gt 20 ]; then
 
     echo "Given number:: $NUM is greater than 20"
 
+elif [ $NUM -eq 20]; then
+
+    echo "Given number:: $NUM is equal to 20"
+
+
 else
     
     echo "Given number:: $NUM is less than 20"
