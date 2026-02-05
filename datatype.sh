4e5305c9e5a648ca1b53108aadffdf6d620a889e
Premnath <premnathb77@gmail.com>
2026-02-05 20:24:32 +0530

    Modified

--- datatype.sh
+++ datatype.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-NO1=$1
-NO2=$2
-
-SUM=$((NO1+NO2))
-SUB=$((NO1-NO2))
-DIV=$((NO1/NO2))
-MUL=$((NO1*NO1))
-
-echo "Output is:: $SUM"
-echo "Output is:: $SUB"
-echo "Output is:: $DIV"
-echo "Output is:: $MUL"
 
 #Array
 
