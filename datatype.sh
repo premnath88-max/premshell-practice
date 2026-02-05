a0ca03528ab2b423892bfe673e650ef6bc6a84c5
Premnath <premnathb77@gmail.com>
2026-02-05 20:13:19 +0530

    Modified

--- datatype.sh
+++ datatype.sh
@@ -4,5 +4,11 @@ NO1=100
 NO2=200
 
 SUM=$((NO1+N02))
+SUB=$((NO1-NO2))
+DIV=$((NO1/NO2))
+MUL=$((NO1*NO1))
 
 echo "Output is:: $SUM"
+echo "Output is:: $SUB"
+echo "Output is:: $DIV"
+echo "Output is:: $MUL"
