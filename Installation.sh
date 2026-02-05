5d0f070d0abc457d40c63d2457a69b40b81571a0
Premnath <premnathb77@gmail.com>
2026-02-05 22:50:56 +0530

    Modified

--- Installation.sh
+++ Installation.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then 
@@ -14,3 +20,13 @@ fi
 
 dnf install nginx -y
 
+
+if [ $? -ne 0 ]; then
+    echo -e "INSTALLING NGINX.......FAILURE $R"
+    exit 1
+
+else
+
+    echo -e "INSTALLING NGINX.......SUCCESS $G"
+
+fi    
