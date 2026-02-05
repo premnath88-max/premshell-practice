38d0a5013e9a2911cc792fc3e9e9f563edd2a987
Premnath <premnathb77@gmail.com>
2026-02-05 22:40:18 +0530

    Modified

--- Installation.sh
+++ Installation.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+if [ $USER -ne 0 ]; then 
+
+    echo "Run this script with root user access"
+
+fi
+
+    echo "Installling Nginx"    
+
+
 dnf install nginx -y
 
