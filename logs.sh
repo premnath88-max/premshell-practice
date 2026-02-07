b2b91f78005a65bd6bb6afb43ed4f442445ea50b
Premnath <premnathb77@gmail.com>
2026-02-07 12:20:17 +0530

    Modified

--- logs.sh
+++ logs.sh
@@ -7,7 +7,7 @@ LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then 
 
-    echo "Run this script with root user access"
+    echo "Run this script with root user access" | tee -a $LOGS_FILE
     exit 1
 
 fi
@@ -16,11 +16,11 @@ mkdir -p $LOGS_FOLDER
 
     VALIDATE(){
         if [ $1 -ne 0 ]; then
-    echo  "$2.......FAILURE"
+    echo  "$2.......FAILURE"    | tee -a $LOGS_FILE
     exit 1
 else
 
-    echo  "$2.......SUCCESS"
+    echo  "$2.......SUCCESS"  | tee -a $LOGS_FILE
 fi    
 
     }
