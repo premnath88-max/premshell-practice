373135171e8f6350978ae4096da88ce08e32c44c
Premnath <premnathb77@gmail.com>
2026-02-07 13:13:06 +0530

    Modified

--- loops.sh
+++ loops.sh
@@ -20,11 +20,11 @@ mkdir -p $LOGS_FOLDER
 
     VALIDATE(){
         if [ $1 -ne 0 ]; then
-    echo -e "$2.......FAILURE $R"    | tee -a $LOGS_FILE
+    echo -e "$2.......$R FAILURE $N"    | tee -a $LOGS_FILE
     exit 1
 else
 
-    echo  "$2.......SUCCESS $G"    | tee -a $LOGS_FILE
+    echo  "$2.......$G SUCCESS $Y"    | tee -a $LOGS_FILE
 fi    
 
     }
@@ -34,7 +34,7 @@ for package in $@
 
 do
 
-    dnf list installed $package $G &>>$LOGS_FILE
+    dnf list installed $package  &>>$LOGS_FILE
 
     if [ $? -ne 0 ]; then
 
@@ -42,7 +42,7 @@ do
         dnf install $package -y &>>$LOGS_FILE
         VALIDATE $? "Installing $package"
     else 
-        echo "$package Already installed,SKIPPING" | tee -a $LOGS_FILE
+        echo "$package Already installed ...$Y SKIPPING $N" | tee -a $LOGS_FILE
 
      fi   
 
